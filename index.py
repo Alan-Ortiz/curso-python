685dec7be6486512dd4d38201ddb134e2b4fd050
Alan-Ortiz <alanrcz93@gmail.com>
2020-03-29 17:15:26 -0400

    File explaining for loop and indexing
    Signed-off-by: Alan-Ortiz <alanrcz93@gmail.com>

---
+++ index.py
@@ -0,0 +1,18 @@
+
+cadena1 = "Hola alan"
+longitud = len(cadena1)
+print(longitud)
+
+rango = range(longitud)
+print(rango)
+for e in rango:
+    print(e)
+
+for e in range(9):
+    print(cadena1) 
+
+for e in range(9):
+    print(cadena1[e]) 
+
+for e in range(9):
+    print(e, cadena1[e]) 
