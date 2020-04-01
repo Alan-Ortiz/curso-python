6708f96958afb4a3ef52eddfe23fbc383f4e1387
Alan.Ortiz <alan.ortiz@ormuco.com>
2020-03-31 23:57:10 -0400

    Adding nested for
    Signed-off-by: Alan.Ortiz <alan.ortiz@ormuco.com>

--- index.py
+++ index.py
@@ -1,18 +1,26 @@
 
-cadena1 = "Hola alan"
-longitud = len(cadena1)
-print(longitud)
+# cadena1 = "Hola alan"
+# longitud = len(cadena1)
+# print(longitud)
 
-rango = range(longitud)
-print(rango)
-for e in rango:
-    print(e)
+# rango = range(longitud)
+# print(rango)
+# for e in rango:
+#     print(e)
 
-for e in range(9):
-    print(cadena1) 
+# for e in range(9):
+#     print(cadena1) 
 
-for e in range(9):
-    print(cadena1[e]) 
+# for e in range(9):
+#     print(cadena1[e]) 
 
-for e in range(9):
-    print(e, cadena1[e]) 
+# for e in range(9):
+#     print(e, cadena1[e]) 
+
+
+for n in range(2, 10):
+    print("n ahora vale: ",n)
+    print("---------------")
+    for x in range(2, n):
+        print ("x ahora vale: ",x)
+        print("---------------")
